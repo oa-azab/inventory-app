626212adfcbf967a2cf92c51620aa061dfa709c2
Omar <oa.azab@gmail.com>
2017-06-18 00:28:04 +0200

    Implement Provider Update Method

--- app/src/main/java/me/azab/oa/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/me/azab/oa/inventoryapp/data/ProductProvider.java
@@ -127,6 +127,48 @@ public class ProductProvider extends ContentProvider {
 
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return updateProduct(uri, values, selection, selectionArgs);
+            case PRODUCT_ID:
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return updateProduct(uri, values, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    private int updateProduct(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        // Data Validation
+        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Product require name");
+        }
+
+        Float price = values.getAsFloat(ProductEntry.COLUMN_PRODUCT_PRICE);
+        if (price == null || price < 0) {
+            throw new IllegalArgumentException("Product price can't be less than zero");
+        }
+
+        Integer quantity = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        if (quantity == null || quantity < 0) {
+            throw new IllegalArgumentException("Product quantity can't be less than zero");
+        }
+
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        // Get SQLite db instance
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Update Pets table
+        int result = db.update(ProductEntry.TABLE_NAME, values, selection, selectionArgs);
+
+        return result;
     }
 }
